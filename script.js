e932a06fd464831b5532ddf467ed450ecbac61e2
Cristaldo Martín <cristaldopmartin@gmail.com>
2023-07-23 04:18:45 -0300

    Página inicio completa - versión 01

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let animado = document.querySelectorAll(".animado");
-let animadoLoad = document.querySelectorAll(".animado-load");
 
 function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
@@ -12,12 +11,35 @@ function mostrarScroll() {
     }
 }
 
-function mostrarLoaded() {
-    for (var i=0; 1 < animadoLoad.length; i++) {
-        animadoLoad[i].style.opacity = 1;
-    }
-}
-
 window.addEventListener('scroll', mostrarScroll);
-window.addEvenListener('load', mostrarLoaded);
+
+// En tu archivo JavaScript
+window.addEventListener('load', function() {
+    // Cuando se cargue la página, obtén todos los elementos con la clase 'fadeIn'
+    const elementosLeft = document.querySelectorAll('.fadeInLeft');
+    const elementosRight = document.querySelectorAll('.fadeInRight')
+  
+    elementosRight.forEach(function(elementoRight, index) {
+        setTimeout(function() {
+          elementoRight.classList.add('active');
+        }, index * 100); // Agrega un pequeño retraso entre cada animación para que parezca más natural
+      });
+    // Agrega la clase 'active' a cada elemento después de un breve momento para iniciar la animación
+    elementosLeft.forEach(function(elementoLeft, index) {
+      setTimeout(function() {
+        elementoLeft.classList.add('active');
+      }, index * 100); // Agrega un pequeño retraso entre cada animación para que parezca más natural
+    });
+  });
+
+window.addEventListener('load', ()=> {
+  texto = document.getElementById("fuente");
+  setTimeout(()=>{
+      texto.classList.add("titulo-efecto");
+  },3500);
+})
+  
+  
+
+
 
